4f77303327c2e48a2c88e2fd391cee493bb6f717
Leandro <leandro@orange.sc>
2018-06-07 15:54:02 -0300

    avanzando con el header

--- dist/templates/header.php
+++ dist/templates/header.php
@@ -1,49 +1,47 @@
 <header class="Header" id="header">
   <div class="container Header__innerWrapper">
-    <div class="Header__logo">
-      <a href="">
-        <h1>Piccadely</h1>
-      </a>
-    </div>
+    <a href="" class="Header__logo">
+      Piccadely
+    </a>
     <div class="Header__menus menus">
-      <div id="first-line">
-        <ul>
-          <li>
-            <a href="">¿cómo pido mi picada?</a>
+      <div class="First-line">
+        <ul class="First-line__list">
+          <li class="First-line__item">
+            <a href="" class="First-line__link">¿cómo pido mi picada?</a>
           </li>
-          <li>
-            <a href="">011 5368 5885</a>
+          <li class="First-line__item">
+            <a href="" class="First-line__link">011 5368 5885</a>
           </li>
-          <li>
-            <a href="" class="but but--rojo but-pedi">pedí online</a>
+          <li class="First-line__item">
+            <a href="" class="but-pedi">pedí online</a>
           </li>
         </ul>
       </div>
-      <nav id="nav">
-        <ul>
-          <li>
-            <a href="">clasicas</a>
+      <nav class="Nav">
+        <ul class="Nav__list">
+          <li class="Nav__item">
+            <a href="" class="Nav__link">clasicas</a>
           </li>
-          <li>
-            <a href="">gourmet</a>
+          <li class="Nav__item">
+            <a href="" class="Nav__link">gourmet</a>
           </li>
-          <li>
-            <a href="">saludable</a>
+          <li class="Nav__item">
+            <a href="" class="Nav__link">saludable</a>
           </li>
-          <li>
-            <a href="">promociones</a>
+          <li class="Nav__item">
+            <a href="" class="Nav__link">promociones</a>
           </li>
-          <li>
-            <a href="">promociones</a>
+          <li class="Nav__item">
+            <a href="" class="Nav__link">promociones</a>
           </li>
-          <li>
-            <a href="">catering</a>
+          <li class="Nav__item">
+            <a href="" class="Nav__link">catering</a>
           </li>
-          <li>
-            <a href="">regalos empresariales</a>
+          <li class="Nav__item">
+            <a href="" class="Nav__link">regalos empresariales</a>
           </li>
-          <li>
-            <a href="">contacto</a>
+          <li class="Nav__item">
+            <a href="" class="Nav__link">contacto</a>
           </li>
         </ul>
       </nav>
