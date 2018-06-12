50f6498e39b7b8829beaea48039c4fdef5cc5b8d
Leandro <leandro@orange.sc>
2018-06-12 11:37:56 -0300

    termino cards y empiezo footer

--- dist/templates/home/picada-de-mes.php
+++ dist/templates/home/picada-de-mes.php
@@ -5,7 +5,7 @@
         <h2 class="title-section Picada-mes__title">las picadas del mes</h2>
       </div>
     </div>
-    <div class="row">
+    <div class="row fila-cards-picada-mes">
       <div class="col-lg-4 card-wrapper">
         <article class="Card">
           <figure class="Card__figure">
@@ -24,19 +24,23 @@
               comen 2 / pican 4
             </div>
             <div class="picada-pedir-precios">
-              <div class="descuento">
-                <span class="off"> 20% off</span><span class="tipo-entrega"> delivery</span>
-              </div>
               <a href="" class="btn-pedi-card">
-                <span class="">$656</span>
+                <div class="off-btn off-price">
+                  <span class="off">20% off</span> 
+                  <span class="tipo-entrega"> delivery</span>
+                </div>
+                <span class="precio-btn">$656</span>
+                <img class="carrito-icon" src="assets/img/cards/carrito-btn.svg" alt="">
               </a>
             </div>
             <div class="picada-pedir-precios">
-              <div class="descuento">
-                <span class="off"> 20% off</span> delivery
-              </div>
-              <a href="" class="">
-                <span class="">$656</span>
+              <a href="" class="btn-pedi-card">
+                <div class="off-btn off-price">
+                  <span class="off">20% off</span> 
+                  <span class="tipo-entrega"> retirás vos</span>
+                </div>
+                <span class="precio-btn">$430</span>
+                <img class="carrito-icon" src="assets/img/cards/carrito-btn.svg" alt="">
               </a>
             </div>
           </div>
@@ -52,10 +56,32 @@
             <a href="">Ver contenido</a>
           </div>
           <div class="Card__info">
-            <div class="picada-nombre"></div>
-            <div class="picada-tamano"></div>
+            <div class="picada-nombre">
+              <h3>cuatro quesos</h3>
+              <span class="precio-card">$135</span>
+            </div>
+            <div class="picada-tamano">
+              comen 2 / pican 4
+            </div>
             <div class="picada-pedir-precios">
-
+              <a href="" class="btn-pedi-card">
+                <div class="off-btn off-price">
+                  <span class="off">20% off</span> 
+                  <span class="tipo-entrega"> delivery</span>
+                </div>
+                <span class="precio-btn">$656</span>
+                <img class="carrito-icon" src="assets/img/cards/carrito-btn.svg" alt="">
+              </a>
+            </div>
+            <div class="picada-pedir-precios">
+              <a href="" class="btn-pedi-card">
+                <div class="off-btn off-price">
+                  <span class="off">20% off</span> 
+                  <span class="tipo-entrega"> retirás vos</span>
+                </div>
+                <span class="precio-btn">$430</span>
+                <img class="carrito-icon" src="assets/img/cards/carrito-btn.svg" alt="">
+              </a>
             </div>
           </div>
         </article>
@@ -70,13 +96,40 @@
             <a href="">Ver contenido</a>
           </div>
           <div class="Card__info">
-            <div class="picada-nombre"></div>
-            <div class="picada-tamano"></div>
+            <div class="picada-nombre">
+              <h3>cuatro quesos</h3>
+              <span class="precio-card">$135</span>
+            </div>
+            <div class="picada-tamano">
+              comen 2 / pican 4
+            </div>
             <div class="picada-pedir-precios">
-
+              <a href="" class="btn-pedi-card">
+                <div class="off-btn off-price">
+                  <span class="off">20% off</span> 
+                  <span class="tipo-entrega"> delivery</span>
+                </div>
+                <span class="precio-btn">$656</span>
+                <img class="carrito-icon" src="assets/img/cards/carrito-btn.svg" alt="">
+              </a>
+            </div>
+            <div class="picada-pedir-precios">
+              <a href="" class="btn-pedi-card">
+                <div class="off-btn off-price">
+                  <span class="off">20% off</span> 
+                  <span class="tipo-entrega"> retirás vos</span>
+                </div>
+                <span class="precio-btn">$430</span>
+                <img class="carrito-icon" src="assets/img/cards/carrito-btn.svg" alt="">
+              </a>
             </div>
           </div>
         </article>
+      </div>
+    </div>
+    <div class="row align-center">
+      <div class="col">
+        <a href="" class="but but-black">ver todas</a>
       </div>
     </div>
   </div>
